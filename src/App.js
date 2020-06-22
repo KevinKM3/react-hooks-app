a889fefbcff0227f54fe2ab09c7b927d3c411f84
Kevin Matthews <kevmatthews3@gmail.com>
2020-06-22 01:12:57 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hooks</p>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add User</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
